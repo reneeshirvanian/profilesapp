3863f97a98f0145fb4a3aa2e3ea0c55ab7bcb250
kellerm2 <142858106+kellerm2@users.noreply.github.com>
2026-01-30 17:13:08 -0500

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,6 +1,9 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+// Imports for send alert
+import { sendAlert } from './functions/send-alert/resource';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
@@ -8,4 +11,13 @@ import { data } from './data/resource';
 defineBackend({
   auth,
   data,
+  sendAlert,
 });
+
+// permission to publish to SNS
+backend.sendAlert.resources.lambda.addToRolePolicy(
+  new PolicyStatement({
+    actions: ['sns:Publish'],
+    resources: ['*'], // put your specific Topic ARN here for stricter security
+  })
+);
